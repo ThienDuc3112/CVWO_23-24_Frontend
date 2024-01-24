30d981af2369b128ec672a32bce8f3bfd06a9a0a
Thien Duc <ntduc4@gmail.com>
2024-01-24 21:16:07 +0800

    Fix API_URL

--- src/costants.ts
+++ src/costants.ts
@@ -1,2 +1,2 @@
-// export const API_URL = "https://cvwo-backend-bljh.onrender.com";
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://cvwo-backend-bljh.onrender.com";
+// export const API_URL = "http://localhost:3000";
